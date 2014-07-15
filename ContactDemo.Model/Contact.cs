ce56f09375ec72dbd342d24cdbac562a75d9d300
DoNormal <robert@versolamorte.nl>
2014-07-15 11:55:53 +0200

    Docs for model classes.

--- ContactDemo.Model/Contact.cs
+++ ContactDemo.Model/Contact.cs
@@ -2,11 +2,29 @@
 
 namespace ContactDemo.Model
 {
+    /// <summary>
+    /// Model for a contact.
+    /// </summary>
     public class Contact
     {
+        /// <summary>
+        /// Gets or sets the identifier of the contact.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the contact.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address of the contact.
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the contact was created.
+        /// </summary>
         public DateTimeOffset WhenCreated { get; set; }
     }
 }
--- ContactDemo.Model/PostContact.cs
+++ ContactDemo.Model/PostContact.cs
@@ -2,11 +2,29 @@
 
 namespace ContactDemo.Model
 {
+    /// <summary>
+    /// Model for a new incoming contact request.
+    /// </summary>
     public class PostContact
     {
+        /// <summary>
+        /// Gets or sets the name of the contact.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email address of the contact.
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the uploaded file.
+        /// </summary>
         public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contents of the uploaded file.
+        /// </summary>
         public Stream FileContents { get; set; }
     }
 }
